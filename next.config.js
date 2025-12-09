dded7f7614d5c8b2cc23c0d5e84de541d56a74d0
siva <siva.l@growx.global>
2025-12-09 17:10:59 +0530

    #SL push

--- next.config.js
+++ next.config.js
@@ -1,40 +1,21 @@
-const path = require("path");
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+// next.config.js
+module.exports = {
   sassOptions: {
-    includePaths: [path.join(__dirname, "styles")],
+    includePaths: [path.join(__dirname, 'styles')],
   },
-
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "res.cloudinary.com",
-        pathname: "**",
-      },
-      {
-        protocol: "https",
-        hostname: "media.dev.to",
-        pathname: "**",
-      },
-      {
-        protocol: "https",
-        hostname: "media2.dev.to",
-        pathname: "**",
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '**',
       },
     ],
   },
-
   typescript: {
     ignoreBuildErrors: true,
   },
-
+  output: "export",
   trailingSlash: true,
   reactStrictMode: false,
-
-  // ❌ REMOVE THIS
-  // eslint: { ignoreDuringBuilds: true },
 };
-
-module.exports = nextConfig;
